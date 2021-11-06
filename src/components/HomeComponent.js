7448231a6cf345d83ddce40347246c00198bc98b
Ege <57564894+EgeBoraErguney@users.noreply.github.com>
2021-11-06 12:15:59 +0300

    fr

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
+
 function HomeComponent() {
-    return (
-      <>
-        <h1>Home Component</h1>
-      </>
-    );
-  }
-  
-  export default HomeComponent;
-  
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let loading = true;
+    fetch("https://back-end.egeboraerguney.workers.dev/posts/")
+      .then((resp) => {
+        if (loading) {
+          console.log(resp);
+        }
+      })
+      .catch((err) => console.log(err));
+    return () => (loading = false);
+  }, []);
+
+  return (
+    <>
+      <h1>Home Component</h1>
+    </>
+  );
+}
+
+export default HomeComponent;
